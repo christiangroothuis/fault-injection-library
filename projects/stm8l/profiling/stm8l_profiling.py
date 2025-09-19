b59f96b959360ec2b9ee4bf999b37432c371712b
christiangroothuis <62361628+christiangroothuis@users.noreply.github.com>
2025-09-19 02:03:57 +0200

    Widen range again

--- projects/stm8l/profiling/stm8l_profiling.py
+++ projects/stm8l/profiling/stm8l_profiling.py
@@ -145,13 +145,13 @@ class Main:
         s_length = 4
         e_length = 200
         length_step = 4
-        s_delay = 4000
-        e_delay = 10000
+        s_delay = 1850
+        e_delay = 1900
         delay_step = 20
-        s_voltage = 1.60
-        e_voltage = 2.20
+        s_voltage = 1.35
+        e_voltage = 1.40
         voltage_step = 0.01
-        n_glitches = 2_000_000
+        n_glitches = 500
 
         exp_id = 0
 
@@ -162,64 +162,61 @@ class Main:
         self.psu.turn_on()
         time.sleep(0.1)
 
-        # for voltage in np.arange(s_voltage, e_voltage + voltage_step, voltage_step):
-        voltage = 1.39
-        print(f"Setting PSU voltage to {voltage:.2f} V")
-        self.psu.set_voltage(voltage)
-        time.sleep(0.1)
+        for voltage in np.arange(s_voltage, e_voltage + voltage_step, voltage_step):
+            print(f"Setting PSU voltage to {voltage:.2f} V")
+            self.psu.set_voltage(voltage)
+            time.sleep(0.1)
 
-        length_band = length_step * 5
-        estimated_optimal_length = round(32 * voltage / 4) * 4
+            length_band = length_step * 4
+            estimated_optimal_length = round(32 * voltage / 4) * 4
 
-        # for length in np.arange(
-        #     min(max(estimated_optimal_length - length_band, s_length), 28),
-        #     estimated_optimal_length + length_band + length_step,
-        #     length_step,
-        # ):
-        length = 32
-        
-        for _ in range(n_glitches):
-            delay = random.randint(s_delay, e_delay)
-            mul_config = {"t1": length, "v1": "VI1"}
-            self.glitcher.arm_multiplexing(delay, mul_config)
-            self.glitcher.reset(0.001)
+            for length in np.arange(
+                min(max(estimated_optimal_length - length_band, s_length), 28),
+                estimated_optimal_length + length_band + length_step,
+                length_step,
+            ):
+                for _ in range(n_glitches):
+                    delay = random.randint(s_delay, e_delay)
+                    mul_config = {"t1": length, "v1": "VI1"}
+                    self.glitcher.arm_multiplexing(delay, mul_config)
+                    self.glitcher.reset(0.001)
 
-            try:
-                self.glitcher.block(timeout=1)
-                time.sleep(0.001)
+                    try:
+                        self.glitcher.block(timeout=1)
+                        time.sleep(0.001)
 
-                success = self.glitcher.read_success_flag()
-                reset = self.glitcher.read_reset_flag()
+                        success = self.glitcher.read_success_flag()
+                        reset = self.glitcher.read_reset_flag()
 
-                if success:
-                    state = b"success"
-                    # send_pushover_notification(
-                    #     user_key=os.getenv("PUSHOVER_USER_KEY"),
-                    #     app_token=os.getenv("PUSHOVER_APP_TOKEN"),
-                    #     message=f"Successful glitch! with delay={delay} ns, length={length} ns, voltage={voltage:.2f} V",
-                    #     title="Successful glitch",
-                    # )
-                elif reset:
-                    state = b"reset"
-                else:
-                    state = b"expected"
-            except:
-                print("[-] Timeout received in block(). Continuing.")
-                self.glitcher.power_cycle_reset(0.2)
-                time.sleep(0.2)
-                state = b"timeout"
+                        if success:
+                            state = b"success"
+                            send_pushover_notification(
+                                user_key=os.getenv("PUSHOVER_USER_KEY"),
+                                app_token=os.getenv("PUSHOVER_APP_TOKEN"),
+                                message=f"Successful glitch! with delay={delay} ns, length={length} ns, voltage={voltage:.2f} V",
+                                title="Successful glitch",
+                            )
+                        elif reset:
+                            state = b"reset"
+                        else:
+                            state = b"expected"
+                    except:
+                        print("[-] Timeout received in block(). Continuing.")
+                        self.glitcher.power_cycle_reset(0.2)
+                        time.sleep(0.2)
+                        state = b"timeout"
 
-            color = self.glitcher.classify(state)
-            self.db.insert(exp_id, voltage * 100, delay, length, color, state)
-            speed = self.glitcher.get_speed(self.start_time, exp_id)
-            experiment_base_id = self.db.get_base_experiments_count()
-            print(
-                self.glitcher.colorize(
-                    f"[+] Experiment {exp_id}\t{experiment_base_id}\t({speed})\t{voltage:.2f}\t{delay:>{len(str(e_delay))}}\t{length}\t{color}\t{state}",
-                    color,
-                )
-            )
-            exp_id += 1
+                    color = self.glitcher.classify(state)
+                    self.db.insert(exp_id, voltage * 100, delay, length, color, state)
+                    speed = self.glitcher.get_speed(self.start_time, exp_id)
+                    experiment_base_id = self.db.get_base_experiments_count()
+                    print(
+                        self.glitcher.colorize(
+                            f"[+] Experiment {exp_id}\t{experiment_base_id}\t({speed})\t{voltage:.2f}\t{delay:>{len(str(e_delay))}}\t{length}\t{color}\t{state}",
+                            color,
+                        )
+                    )
+                    exp_id += 1
 
         self.psu.turn_off()
 
