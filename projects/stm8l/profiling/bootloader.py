bf742b2a4710165d6c8b9a574d2620f189f5fe80
christiangroothuis <62361628+christiangroothuis@users.noreply.github.com>
2025-09-20 12:14:54 +0200

    Move bootloader script to profiling and clean up

--- projects/stm8l/profiling/bootloader.py
+++ projects/stm8l/profiling/bootloader.py
@@ -8,26 +8,23 @@ import time
 from dotenv import load_dotenv
 
 from findus import Database, PicoGlitcher
-from projects.stm8l.utils.psu import PS3005D
 from projects.stm8l.utils.pushover import send_pushover_notification
+from ..utils.psu import PS3005D
 
-RX_PIN = 19
+RX_PIN = 27
 
 
-class BootloaderGlitcher(PicoGlitcher):
+class BootloaderProfilingGlitcher(PicoGlitcher):
     def init(self, *args, **kwargs):
         super().init(*args, **kwargs)
 
         self.pico_glitcher.pyb.exec_raw_no_follow(
-            "import machine\n" f"adc = machine.ADC(machine.Pin({RX_PIN}))\n"
+            "import machine\n" f"adc = machine.ADC({RX_PIN})\n"
         )
 
-    def read_success(self) -> bool:
-        # adc input should be around 9000 for success
-        # its normally around 400-500
-        out = self.pico_glitcher.pyb.exec(
-            "print(adc.read_u16())\n"
-        )
+    def read_success_flag(self) -> bool:
+        out = self.pico_glitcher.pyb.exec_raw(f"print(int(adc.read_u16()))\n")
+
         return int(out[0].strip()) > 8000
 
     def classify(self, state: bytes) -> str:
@@ -45,7 +42,7 @@ class Main:
     def __init__(self, args):
         self.args = args
 
-        self.glitcher = BootloaderGlitcher()
+        self.glitcher = BootloaderProfilingGlitcher()
         self.glitcher.init(port=args.rpico, enable_vtarget=False)
         self.glitcher.change_config_and_reset("mux_vinit", "3.3")
         self.glitcher.init(port=args.rpico, enable_vtarget=False)
@@ -62,17 +59,14 @@ class Main:
             column_names=["voltage", "delay", "length"],
         )
         self.start_time = int(time.time())
-
         self.psu = PS3005D(port=args.psu)
 
     def run(self):
-        s_length = 4
-        e_length = 200
-        s_delay = 1850
-        e_delay = 1900
-        voltage = 1.39
-        n_glitches = 500
-
+        s_length = 28
+        e_length = 28
+        s_delay = 25000
+        e_delay = 35000
+        voltage = 1.22
         exp_id = 0
 
         self.psu.set_voltage(voltage)
@@ -82,9 +76,9 @@ class Main:
         self.psu.turn_on()
         time.sleep(0.1)
 
-        for _ in range(n_glitches):
-            length = random.randint(s_length, e_length)
-            delay = random.randint(s_delay, e_delay)
+        while True:
+            delay = int(random.randint(s_delay, e_delay))
+            length = int(random.randint(s_length, e_length))
             mul_config = {"t1": length, "v1": "VI1"}
             self.glitcher.arm_multiplexing(delay, mul_config)
             self.glitcher.reset(100e-6)  # reset for 100us
@@ -92,18 +86,17 @@ class Main:
 
             try:
                 self.glitcher.block(timeout=1)
-                time.sleep(60e-6)  # wait for USART_RX pin to go high
-
-                success = self.glitcher.read_success()
+                time.sleep(70e-6)
+                success = self.glitcher.read_success_flag()
 
                 if success:
-                    state = b"success"
                     send_pushover_notification(
                         user_key=os.getenv("PUSHOVER_USER_KEY"),
                         app_token=os.getenv("PUSHOVER_APP_TOKEN"),
                         message=f"Successful glitch! with delay={delay} ns, length={length} ns, voltage={voltage:.2f} V",
                         title="Successful glitch",
                     )
+                    state = b"success"
                 else:
                     state = b"expected"
             except:
@@ -125,8 +118,6 @@ class Main:
             )
             exp_id += 1
 
-        self.psu.turn_off()
-
 
 if __name__ == "__main__":
     load_dotenv()
@@ -142,20 +133,6 @@ if __name__ == "__main__":
     )
     p.add_argument(
         "--psu", default="/dev/ttyUSB0", required=True, help="PSU serial port"
-    )
-    p.add_argument(
-        "--reset-hold", type=float, default=0.01, help="Target reset hold time (s)"
-    )
-    p.add_argument(
-        "--block-timeout",
-        type=float,
-        default=1.0,
-        help="Timeout waiting for glitch (s)",
-    )
-    p.add_argument(
-        "--trigger-input",
-        default="default",
-        help="The trigger input to use (default, alt, ext1, ext2). The inputs ext1 and ext2 require the PicoGlitcher v2.",
     )
     p.add_argument("--resume", action="store_true", help="Resume previous database run")
     p.add_argument(
