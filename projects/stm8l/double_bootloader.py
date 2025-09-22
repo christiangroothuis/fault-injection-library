bbae43ffaed86df26475b7b4475622481815ec75
christiangroothuis <62361628+christiangroothuis@users.noreply.github.com>
2025-09-22 17:30:17 +0200

    Debug

--- projects/stm8l/double_bootloader.py
+++ projects/stm8l/double_bootloader.py
@@ -172,7 +172,8 @@ class Main:
                     state = b"success"
                 else:
                     state = b"expected"
-            except Exception:
+            except Exception as e:
+                print(e)
                 print("[-] Timeout received in block(). Continuing.")
                 self.glitcher.power_cycle_reset(0.2)
                 time.sleep(0.2)
