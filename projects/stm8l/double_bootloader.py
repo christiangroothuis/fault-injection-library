4b9abcd26cdf6cd9f230fb931d4babf7e621bedf
christiangroothuis <62361628+christiangroothuis@users.noreply.github.com>
2025-09-22 17:57:32 +0200

    Debug

--- projects/stm8l/double_bootloader.py
+++ projects/stm8l/double_bootloader.py
@@ -185,8 +185,8 @@ class Main:
             try:
                 self.glitcher.block(timeout=1)
                 time.sleep(100e-6)  # wait for rx to go high if success
-                self.mux.enable_uart_tx_alt0()
                 success = self.glitcher.read_success_flag()
+                self.mux.enable_uart_tx_alt0()
                 print(
                     self.glitcher.pico_glitcher.pyb.exec_raw(
                         f"print(int(adc.read_u16()))\n"
