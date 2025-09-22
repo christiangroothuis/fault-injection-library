502d288ee151a5a24f110e943acf2f996fa7c81c
christiangroothuis <62361628+christiangroothuis@users.noreply.github.com>
2025-09-22 16:26:27 +0200

    Try custom bl class

--- projects/stm8l/double_bootloader.py
+++ projects/stm8l/double_bootloader.py
@@ -363,7 +363,7 @@ class Main:
                     state = b"success"
                 else:
                     state = b"expected"
-            except BlockTimeoutError:
+            except:
                 print("[-] Timeout received in block(). Continuing.")
                 self.glitcher.power_cycle_reset(0.2)
                 time.sleep(0.2)
