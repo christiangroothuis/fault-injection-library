a3547be2948922af7276920b060236b4bad6f136
christiangroothuis <62361628+christiangroothuis@users.noreply.github.com>
2025-09-23 09:35:58 +0200

    Random profiling

--- projects/stm8l/profiling/profiling.py
+++ projects/stm8l/profiling/profiling.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import os
 import random
 import sys
 import time
@@ -10,6 +11,7 @@ from dotenv import load_dotenv
 
 from findus import Database, PicoGlitcher
 from ..utils.psu import PS3005D
+from ..utils.pushover import send_pushover_notification
 
 SUCCESS_PIN = 20
 RESET_PIN = 21
@@ -48,16 +50,16 @@ class Main:
     def __init__(self, args):
         self.args = args
         self.parameters = {
-            "s_length": 24,
-            "e_length": 100,
+            "s_length": 4,
+            "e_length": 300,
             "length_step": 4,
             "s_delay": 1850,
             "e_delay": 1920,
             "delay_step": 16,
-            "s_voltage": 0.90,
+            "s_voltage": 0.20,
             "e_voltage": 2.20,
             "voltage_step": 0.01,
-            "n_glitches": 500,
+            "n_glitches": 100_000,
         }
 
         self.glitcher = ProfilingGlitcher()
@@ -98,54 +100,59 @@ class Main:
             self.psu.set_voltage(voltage)
             time.sleep(0.1)
 
-            for length in np.arange(
-                self.parameters["s_length"],
-                self.parameters["e_length"],
-                self.parameters["length_step"],
-            ):
-                for delay in range(
-                    self.parameters["s_delay"], self.parameters["e_delay"], self.parameters["delay_step"]
-                ):
-                    for _ in range(self.parameters["n_glitches"]):
-                        length = round(length / 4) * 4  # ensure length is multiple of 4
-                        delay = round(delay / 4) * 4  # ensure delay is multiple of 4
+            delay = random.randint(
+                self.parameters["s_delay"], self.parameters["e_delay"]
+            )
+            length = random.randint(
+                self.parameters["s_length"], self.parameters["e_length"]
+            )
 
-                        self.glitcher.arm_double_multiplexing(delay, length, "VI1", delay + length + 300, length, "3.3")
-                        self.glitcher.reset(200e-6)  # reset for 50us
-                        success = False
+            length = round(length / 4) * 4  # ensure length is multiple of 4
+            delay = round(delay / 4) * 4  # ensure delay is multiple of 4
 
-                        try:
-                            self.glitcher.block(timeout=0.1)
-                            time.sleep(60e-6)
-                            success = self.glitcher.read_success_flag()
-                            reset = self.glitcher.read_reset_flag()
+            self.glitcher.arm_double_multiplexing(
+                delay, length, "VI1", delay + length + 300, length, "3.3"
+            )
+            self.glitcher.reset(200e-6)  # reset for 50us
+            success = False
 
-                            if success:
-                                state = b"success"
-                            elif reset:
-                                state = b"reset"
-                            else:
-                                state = b"expected"
-                        except:
-                            print("[-] Timeout received in block(). Continuing.")
-                            self.glitcher.power_cycle_reset(0.2)
-                            time.sleep(0.2)
-                            state = b"timeout"
+            try:
+                self.glitcher.block(timeout=0.1)
+                time.sleep(60e-6)
+                success = self.glitcher.read_success_flag()
+                reset = self.glitcher.read_reset_flag()
 
-                        color = self.glitcher.classify(state)
-                        if success:
-                            self.db.insert(
-                                exp_id, voltage * 100, delay, length, color, state
-                            )
-                        speed = self.glitcher.get_speed(self.start_time, exp_id)
-                        experiment_base_id = self.db.get_base_experiments_count()
-                        print(
-                            self.glitcher.colorize(
-                                f"[+] Experiment {exp_id}\t{experiment_base_id}\t({speed})\t{voltage:.2f}\t{delay:>{len(str(self.parameters['e_delay']))}}\t{length}\t{color}\t{state}",
-                                color,
-                            )
-                        )
-                        exp_id += 1
+                if success:
+                    state = b"success"
+
+                    send_pushover_notification(
+                        user_key=os.getenv("PUSHOVER_USER_KEY"),
+                        app_token=os.getenv("PUSHOVER_APP_TOKEN"),
+                        message=f"Successful glitch! with delays={delay} ns, length={length} ns, voltage={self.parameters['voltage']:.2f} V",
+                        title="Successful glitch",
+                    )
+                elif reset:
+                    state = b"reset"
+                else:
+                    state = b"expected"
+            except:
+                print("[-] Timeout received in block(). Continuing.")
+                self.glitcher.power_cycle_reset(0.2)
+                time.sleep(0.2)
+                state = b"timeout"
+
+            color = self.glitcher.classify(state)
+            if state != b"expected":
+                self.db.insert(exp_id, voltage * 100, delay, length, color, state)
+            speed = self.glitcher.get_speed(self.start_time, exp_id)
+            experiment_base_id = self.db.get_base_experiments_count()
+            print(
+                self.glitcher.colorize(
+                    f"[+] Experiment {exp_id}\t{experiment_base_id}\t({speed})\t{voltage:.2f}\t{delay:>{len(str(self.parameters['e_delay']))}}\t{length}\t{color}\t{state}",
+                    color,
+                )
+            )
+            exp_id += 1
 
 
 if __name__ == "__main__":
