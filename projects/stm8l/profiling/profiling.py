46a54dd2bf1b2db7d7c9e612663aa234047ca08b
christiangroothuis <62361628+christiangroothuis@users.noreply.github.com>
2025-10-06 16:37:25 +0200

    Update profiling scripts for new glitcher

--- projects/stm8l/profiling/profiling.py
+++ projects/stm8l/profiling/profiling.py
@@ -2,6 +2,7 @@
 
 import argparse
 import os
+from projects.stm8l.firmware.glitcher import GlitcherClient
 import random
 import sys
 import time
@@ -13,28 +14,8 @@ from findus import Database, PicoGlitcher
 from ..utils.psu import PS3005D
 from ..utils.pushover import send_pushover_notification
 
-SUCCESS_PIN = 20
-RESET_PIN = 21
-
 
 class ProfilingGlitcher(PicoGlitcher):
-    def init(self, *args, **kwargs):
-        super().init(*args, **kwargs)
-
-        self.pico_glitcher.pyb.exec_raw_no_follow(
-            "import machine\n"
-            f"success_pin = machine.Pin({SUCCESS_PIN}, machine.Pin.IN, machine.Pin.PULL_DOWN)\n"
-            f"reset_pin = machine.Pin({RESET_PIN}, machine.Pin.IN, machine.Pin.PULL_DOWN)\n"
-        )
-
-    def read_success_flag(self) -> bool:
-        out = self.pico_glitcher.pyb.exec_raw(f"print(int(success_pin.value()))\n")
-        return bool(int(out[0].strip()))
-
-    def read_reset_flag(self) -> bool:
-        out = self.pico_glitcher.pyb.exec_raw(f"print(int(reset_pin.value()))\n")
-        return bool(int(out[0].strip()))
-
     def classify(self, state: bytes) -> str:
         color = "C"
         if b"expected" in state:
@@ -43,6 +24,10 @@ class ProfilingGlitcher(PicoGlitcher):
             color = "Y"
         elif b"success" in state:
             color = "R"
+        elif b"bor_reset" in state:
+            color = "B"
+        elif b"por_reset" in state:
+            color = "M"
         return color
 
 
@@ -50,25 +35,26 @@ class Main:
     def __init__(self, args):
         self.args = args
         self.parameters = {
-            "s_length": 4,
-            "e_length": 200,
+            "s_length": args.length[0],
+            "e_length": args.length[1],
             "length_step": 4,
-            "s_delay": 1860,
-            "e_delay": 1900,
-            "delay_step": 16,
-            "s_voltage": 0.20,
-            "e_voltage": 2.20,
+            "s_delay": 1876,
+            "e_delay": 1892,
+            "s_voltage": args.voltage[0],
+            "e_voltage": args.voltage[1],
             "voltage_step": 0.01,
-            "n_glitches": 10_000,
+            "n_glitches": 500,
         }
 
-        self.glitcher = ProfilingGlitcher()
-        self.glitcher.init(port=args.rpico, enable_vtarget=False)
-        self.glitcher.change_config_and_reset("mux_vinit", "3.3")
-        self.glitcher.init(port=args.rpico, enable_vtarget=False)
+        self.glitcher = GlitcherClient(args.rpico)
+        self.glitcher.open()
+        self.findus_glitcher = ProfilingGlitcher()
+        # self.glitcher.init(port=args.rpico, enable_vtarget=False)
+        # self.glitcher.change_config_and_reset("mux_vinit", "3.3")
+        # self.glitcher.init(port=args.rpico, enable_vtarget=False)
 
-        self.glitcher.rising_edge_trigger()
-        self.glitcher.set_multiplexing()
+        # self.glitcher.rising_edge_trigger()
+        # self.glitcher.set_multiplexing()
 
         self.glitcher.power_cycle_reset(0.01)
 
@@ -91,64 +77,86 @@ class Main:
         self.psu.turn_on()
         time.sleep(0.1)
 
-        while True:
-            voltage = round(random.uniform(self.parameters["s_voltage"], self.parameters["e_voltage"]), 2)
+        for voltage in np.arange(
+            self.parameters["s_voltage"],
+            self.parameters["e_voltage"] + self.parameters["voltage_step"],
+            self.parameters["voltage_step"],
+        ):
             print(f"Setting PSU voltage to {voltage:.2f} V")
             self.psu.set_voltage(voltage)
             time.sleep(0.1)
 
-            for _ in range(self.parameters["n_glitches"]):
-                delay = random.randint(
-                    self.parameters["s_delay"], self.parameters["e_delay"]
-                )
-                length = random.randint(
-                    self.parameters["s_length"], self.parameters["e_length"]
-                )
-                length = round(length / 4) * 4  # ensure length is multiple of 4
-                delay = round(delay / 4) * 4  # ensure delay is multiple of 4
-
-                self.glitcher.arm_double_multiplexing(delay, length, "VI1", delay + length + 100, length, "3.3")
-                # self.glitcher.arm_multiplexing(delay, {"t1": length, "v1": "VI1"})
-                self.glitcher.reset(200e-6)  # reset for 50us
-                success = False
-
-                try:
-                    self.glitcher.block(timeout=0.1)
-                    time.sleep(400e-6)
-                    success = self.glitcher.read_success_flag()
-                    reset = self.glitcher.read_reset_flag()
-
-                    if success:
-                        state = b"success"
-
-                        send_pushover_notification(
-                            user_key=os.getenv("PUSHOVER_USER_KEY"),
-                            app_token=os.getenv("PUSHOVER_APP_TOKEN"),
-                            message=f"Successful glitch! with delays={delay} ns, length={length} ns, voltage={voltage:.2f} V",
-                            title="Successful glitch",
-                        )
-                    elif reset:
-                        state = b"reset"
-                    else:
-                        state = b"expected"
-                except TimeoutError:
-                    print("[-] Timeout received in block(). Continuing.")
-                    self.glitcher.power_cycle_reset(0.2)
-                    time.sleep(0.2)
-                    state = b"timeout"
-
-                color = self.glitcher.classify(state)
-                if state != b"expected":
-                    self.db.insert(exp_id, voltage * 100, delay, length, color, state)
-                speed = self.glitcher.get_speed(self.start_time, exp_id)
-                experiment_base_id = self.db.get_base_experiments_count()
-                print(
-                    self.glitcher.colorize(
-                        f"[+] Experiment {exp_id}\t{experiment_base_id}\t({speed})\t{voltage:.2f}\t{delay:>{len(str(self.parameters['e_delay']))}}\t{length}\t{color}\t{state}",
-                        color,
+            for length in range(
+                self.parameters["s_length"],
+                self.parameters["e_length"] + 1,
+                self.parameters["length_step"],
+            ):
+                for _ in range(self.parameters["n_glitches"]):
+                    delay = random.randint(
+                        self.parameters["s_delay"], self.parameters["e_delay"]
                     )
-                )
-                exp_id += 1
+                    delay = round(delay / 4) * 4  # ensure delay is multiple of 4
+
+                    self.glitcher.arm_double_multiplexing(
+                        delay, 24, "VI1", delay + length + 100, length, "3.3"
+                    )
+                    # self.glitcher.arm_multiplexing(delay, {"t1": length, "v1": "VI1"})
+                    self.glitcher.reset(50)  # reset for 50us
+                    # self.glitcher.reset(200e-6)  # reset for 50us
+
+                    try:
+                        # self.glitcher.block(timeout=0.1)
+                        self.glitcher.wait_done(1)
+
+                        result = self.glitcher.pinstat()
+
+                        success = result["success"]
+                        bor_reset = result["bor"]
+                        por_reset = result["por"]
+
+                        if success:
+                            state = b"success"
+
+                            send_pushover_notification(
+                                message=f"Successful glitch! with delays={delay} ns, length={length} ns, voltage={voltage:.2f} V",
+                                title="Successful glitch",
+                            )
+                        elif bor_reset:
+                            state = b"bor_reset"
+                        elif por_reset:
+                            state = b"por_reset"
+                        else:
+                            state = b"expected"
+                    except TimeoutError:
+                        print("[-] Timeout received in block(). Continuing.")
+                        self.glitcher.power_cycle_reset(0.2)
+                        time.sleep(0.2)
+                        state = b"timeout"
+
+                    color = self.findus_glitcher.classify(state)
+                    if state != b"expected":
+                        self.db.insert(
+                            exp_id,
+                            voltage * 100,
+                            delay,
+                            length,
+                            color,
+                            state,
+                            commit=False,
+                        )
+
+                    if exp_id % 1000 == 0:
+                        self.db.con.commit()
+
+                    speed = self.findus_glitcher.get_speed(self.start_time, exp_id)
+                    experiment_base_id = self.db.get_base_experiments_count()
+                    print(
+                        self.findus_glitcher.colorize(
+                            f"[+] Experiment {exp_id}\t{experiment_base_id}\t({speed})\t{voltage:.2f}\t{delay:>{len(str(self.parameters['e_delay']))}}\t{length}\t{color}\t{state}",
+                            color,
+                        )
+                    )
+                    exp_id += 1
 
 
 if __name__ == "__main__":
@@ -172,6 +180,8 @@ if __name__ == "__main__":
     )
     p.add_argument("--ic", required=True, help="IC number")
     p.add_argument("--bor", help="Brownout reset voltage enabled")
+    p.add_argument("--length", required=True, nargs=2, help="length start and end", type=int)
+    p.add_argument("--voltage", required=True, nargs=2, help="voltage start and end", type=float)
     args = p.parse_args()
 
     try:
--- projects/stm8l/profiling/random_profiling.py
+++ projects/stm8l/profiling/random_profiling.py
@@ -1,36 +1,20 @@
 #!/usr/bin/env python3
 
 import argparse
-import logging
+from projects.stm8l.firmware.glitcher import GlitcherClient
 import random
 import sys
 import time
+import numpy as np
+
 from dotenv import load_dotenv
 
 from findus import Database, PicoGlitcher
+from ..utils.psu import PS3005D
+from ..utils.pushover import send_pushover_notification
 
-SUCCESS_PIN = 20
-EXPECTED_PIN = 21
 
-
-class DerivedPicoGlitcher(PicoGlitcher):
-    def init(self, *args, **kwargs):
-        super().init(*args, **kwargs)
-
-        self.pico_glitcher.pyb.exec_raw_no_follow(
-            "import machine\n"
-            f"success_pin = machine.Pin({SUCCESS_PIN}, machine.Pin.IN, machine.Pin.PULL_DOWN)\n"
-            f"expected_pin = machine.Pin({EXPECTED_PIN}, machine.Pin.IN, machine.Pin.PULL_DOWN)\n"
-        )
-
-    def read_success_flag(self) -> bool:
-        out = self.pico_glitcher.pyb.exec_raw(f"print(int(success_pin.value()))\n")
-        return bool(int(out[0].strip()))
-
-    def read_expected_flag(self) -> bool:
-        out = self.pico_glitcher.pyb.exec_raw(f"print(int(expected_pin.value()))\n")
-        return bool(int(out[0].strip()))
-
+class ProfilingGlitcher(PicoGlitcher):
     def classify(self, state: bytes) -> str:
         color = "C"
         if b"expected" in state:
@@ -39,78 +23,123 @@ class DerivedPicoGlitcher(PicoGlitcher):
             color = "Y"
         elif b"success" in state:
             color = "R"
+        elif b"bor_reset" in state:
+            color = "B"
+        elif b"por_reset" in state:
+            color = "P"
         return color
 
 
 class Main:
     def __init__(self, args):
         self.args = args
+        self.parameters = {
+            "s_length": args.length[0],
+            "e_length": args.length[1],
+            "s_delay": 1876,
+            "e_delay": 1892,
+            "s_voltage": args.voltage[0],
+            "e_voltage": args.voltage[1],
+            "voltage_step": 0.01,
+            "n_glitches": 10000,
+        }
 
-        self.glitcher = DerivedPicoGlitcher()
-        self.glitcher.init(port=args.rpico, enable_vtarget=False)
-        self.glitcher.change_config_and_reset("mux_vinit", "3.3")
-        self.glitcher.init(port=args.rpico, enable_vtarget=False)
-
-        self.glitcher.rising_edge_trigger()
-
-        self.glitcher.set_multiplexing()
+        self.glitcher = GlitcherClient(args.rpico)
         self.glitcher.power_cycle_reset(0.01)
+        self.findus_glitcher = ProfilingGlitcher()
 
-        self.db = Database(sys.argv, resume=args.resume, nostore=args.no_store)
+        self.db = Database(
+            sys.argv + [f"{k}={v}" for k, v in self.parameters.items()],
+            resume=args.resume,
+            nostore=args.no_store,
+            column_names=["voltage", "delay", "length"],
+        )
         self.start_time = int(time.time())
+        self.psu = PS3005D(port=args.psu)
 
     def run(self):
-        s_length = self.args.length[0]
-        e_length = self.args.length[1]
-        s_delay = self.args.delay[0]
-        e_delay = self.args.delay[1]
-
         exp_id = 0
 
+        self.psu.set_voltage(self.parameters["s_voltage"])
+        time.sleep(0.1)
+        self.psu.set_current_limit(0.2)
+        time.sleep(0.1)
+        self.psu.turn_on()
+        time.sleep(0.1)
+
+        voltages = np.arange(
+            self.parameters["s_voltage"],
+            self.parameters["e_voltage"] + self.parameters["voltage_step"],
+            self.parameters["voltage_step"],
+        )
+
         while True:
-            if exp_id % 1000 == 0:
-                self.glitcher.power_cycle_reset(1)
-
-            delay = random.randint(s_delay, e_delay)
-            length = random.randint(s_length, e_length)
-
-            mul_config = {"t1": length, "v1": "1.8"}
-            self.glitcher.arm_multiplexing(delay, mul_config)
-
-            self.glitcher.reset(0.01)
-
-            try:
-                self.glitcher.block(timeout=1)
-
-                time.sleep(0.001)
-
-                success = self.glitcher.read_success_flag()
-                reset = self.glitcher.read_expected_flag()
-                print(f"success={success}, reset={reset}")
-
-                if success:
-                    state = b"success"
-                elif reset:
-                    state = b"reset"
-                else:
-                    state = b"expected"
-            except:
-                print("[-] Timeout received in block(). Continuing.")
-                self.glitcher.power_cycle_reset(0.2)
-                time.sleep(0.2)
-                state = b"timeout"
-
-            color = self.glitcher.classify(state)
-            self.db.insert(exp_id, delay, length, color, state)
-            speed = self.glitcher.get_speed(self.start_time, exp_id)
-            experiment_base_id = self.db.get_base_experiments_count()
-            print(
-                self.glitcher.colorize(
-                    f"[+] Experiment {exp_id}\t{experiment_base_id}\t({speed})\t{delay:>{len(str(e_delay))}}\t{length}\t{color}\t{state}",
-                    color,
+            voltage = random.choice(voltages)
+            print(f"Setting PSU voltage to {voltage:.2f} V")
+            self.psu.set_voltage(voltage)
+            time.sleep(0.1)
+            for _ in range(self.parameters["n_glitches"]):
+                length = random.randint(
+                    self.parameters["s_length"], self.parameters["e_length"]
                 )
-            )
-            exp_id += 1
+                delay = random.randint(
+                    self.parameters["s_delay"], self.parameters["e_delay"]
+                )
+                length = round(length / 4) * 4  # ensure length is multiple of 4
+                delay = round(delay / 4) * 4  # ensure delay is multiple of 4
+
+                self.glitcher.arm_double_multiplexing(
+                    delay, length, "VI1", delay + length + 100, length, "3.3"
+                )
+
+                self.glitcher.reset(50)
+                success = False
+
+                try:
+                    self.glitcher.wait_done(0.1)
+                    time.sleep(50e-6)
+                    pin_state = self.glitcher.pinstat()
+                    success = pin_state["success"]
+                    bor_reset = pin_state["bor"]
+                    por_reset = pin_state["por"]
+
+                    if success:
+                        state = b"success"
+
+                        send_pushover_notification(
+                            message=f"Successful glitch! with delays={delay} ns, length={length} ns, voltage={voltage:.2f} V",
+                            title="Successful glitch",
+                        )
+                    elif bor_reset:
+                        state = b"bor_reset"
+                    elif por_reset:
+                        state = b"por_reset"
+                    else:
+                        state = b"expected"
+                except TimeoutError:
+                    print("[-] Timeout received in block(). Continuing.")
+                    self.glitcher.power_cycle_reset(0.2)
+                    time.sleep(0.01)
+                    state = b"timeout"
+
+                color = self.findus_glitcher.classify(state)
+                if state != b"expected":
+                    self.db.insert(
+                        exp_id, voltage * 100, delay, length, color, state, commit=False
+                    )
+
+                if exp_id % 1000 == 0:
+                    self.db.con.commit()
+
+                speed = self.findus_glitcher.get_speed(self.start_time, exp_id)
+                experiment_base_id = self.db.get_base_experiments_count()
+                print(
+                    self.findus_glitcher.colorize(
+                        f"[+] Experiment {exp_id}\t{experiment_base_id}\t({speed})\t{voltage:.2f}\t{delay:>{len(str(self.parameters['e_delay']))}}\t{length}\t{color}\t{state}",
+                        color,
+                    )
+                )
+                exp_id += 1
 
 
 if __name__ == "__main__":
@@ -119,34 +148,29 @@ if __name__ == "__main__":
     p = argparse.ArgumentParser(
         description="STM8L single-glitch via external trigger + success pin"
     )
-    p.add_argument("--rpico", default="/dev/ttyUSB1", help="PicoGlitcher serial port")
     p.add_argument(
-        "--delay", nargs=2, type=int, required=True, help="Glitch offset range (ns)"
-    )
-    p.add_argument(
-        "--length",
-        nargs=2,
-        type=int,
+        "--rpico",
+        default="/dev/ttyUSB1",
         required=True,
-        help="Glitch pulse width range (ns)",
+        help="PicoGlitcher serial port",
     )
     p.add_argument(
-        "--reset-hold", type=float, default=0.01, help="Target reset hold time (s)"
-    )
-    p.add_argument(
-        "--block-timeout",
-        type=float,
-        default=1.0,
-        help="Timeout waiting for glitch (s)",
-    )
-    p.add_argument(
-        "--trigger-input",
-        default="default",
-        help="The trigger input to use (default, alt, ext1, ext2). The inputs ext1 and ext2 require the PicoGlitcher v2.",
+        "--psu", default="/dev/ttyUSB0", required=True, help="PSU serial port"
     )
     p.add_argument("--resume", action="store_true", help="Resume previous database run")
     p.add_argument(
         "--no-store", action="store_true", help="Do not write results to the database"
+    )
+    p.add_argument("--ic", required=True, help="IC number")
+    p.add_argument("--bor", help="Brownout reset voltage enabled")
+    p.add_argument(
+        "--shape", choices=["clean", "ringing"], default="clean", help="Glitch shape"
+    )
+    p.add_argument(
+        "--length", required=True, nargs=2, help="length start and end", type=int
+    )
+    p.add_argument(
+        "--voltage", required=True, nargs=2, help="voltage start and end", type=float
     )
     args = p.parse_args()
 
